950b6130fe9254ca1b4bbdec1689c9086d9738bb
unknown <asmanahasfree2@gmail.com>
2020-04-27 10:57:29 +0300

    Procfile for Heroku

--- database/migrations/2020_04_18_131644_create_article_table.php
+++ database/migrations/2020_04_18_131644_create_article_table.php
@@ -16,7 +16,7 @@ class CreateArticleTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->longText('body')->nullable()->change();
+            $table->longText('body');
             $table->string('image');
             $table->timestamps();
         });
